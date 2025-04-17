9b14a81c4cfbaf3163bbcc807d62514c7b8e9b33
Mark Locklear <marklocklear@gmail.com>
2025-04-17 12:01:35 -0400

    add auth and admin functionality

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,14 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_admin
+
   def index
     @questions = Question.order(created_at: :desc).page(params[:page]).per(10)
+  end
+
+  private
+
+  def require_admin
+    redirect_to root_path, alert: 'Not authorized' unless current_user&.admin?
   end
 end
